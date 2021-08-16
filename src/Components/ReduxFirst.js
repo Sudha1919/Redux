ec8f6022b0e6bd841e749ec6bc6a5fd2d09569c9
Sudha1919 <sudhabangaru1919@gmail.com>
2021-08-16 12:48:06 +0530

    2 files modified

--- src/Components/ReduxFirst.js
+++ src/Components/ReduxFirst.js
@@ -2,8 +2,10 @@ import React from 'react';
 import { connect } from 'react-redux';
 
 const ReduxFirst = props => {
-  console.log(props);
-  return <h1>First Redux Component : {props.Name}</h1>;
+  console.log(props.Employees[0]);
+  return <h1>First Redux Component : {props.Employees.map((data)=>{
+    <span>{data[0]}</span>
+  })} </h1>;
 };
 
 const mapStateToProps = state => {
--- src/reducer/reducer1.js
+++ src/reducer/reducer1.js
@@ -1,4 +1,11 @@
-function reducer1(state = { Name: 'Sudha' }) {
+function reducer1(
+  state = {
+    Employees: [
+      { Name: 'Sudha', Age: 28, Designation: 'UI Developer' },
+      { Name: 'Sandeep', Age: 32, Designation: 'DBA' }
+    ]
+  }
+) {
   return state;
 }
 export default reducer1;
