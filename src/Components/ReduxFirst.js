937680104eb44d31820139dd0ebbb6af8afe7579
Sudha1919 <sudhabangaru1919@gmail.com>
2021-08-17 09:40:16 +0530

    1 file modified

--- src/Components/ReduxFirst.js
+++ src/Components/ReduxFirst.js
@@ -3,13 +3,24 @@ import { connect } from 'react-redux';
 
 const ReduxFirst = props => {
   console.log(props.Employees[1]);
-  console.log(props)
+  console.log(props);
   return (
     <>
-      <h1>First Redux Component : {props.Employees.map(({Name, Age, Designation}) =>(
-        <ul><li>{Name} {Age} {Designation}</li><br></br>
-        </ul>
-      ))} </h1>
+      <h1>First Redux Component :</h1>
+      <table border="1">
+        <tr>
+          <th>Name</th>
+          <th>Age</th>
+          <th>Designation</th>
+        </tr>
+        {props.Employees.map(({ Name, Age, Designation }) => (
+          <tr>
+            <td>{Name}</td>
+            <td> {Age}</td>
+            <td>{Designation}</td>
+          </tr>
+        ))}
+      </table>
     </>
   );
 };
